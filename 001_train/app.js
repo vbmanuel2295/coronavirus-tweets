bbda2cf67ee741c4d729a6d240b25307f96fd5e9
Vince <it2@solutionsone.co>
2021-04-22 22:25:47 +0800

    Implemented train and test ML for coronavirus tweets

---
+++ 001_train/app.js
@@ -0,0 +1,50 @@
+/**
+ * This module is used for training a coronavirus tweets classifier.
+ *
+ * 1. It loads data from the dataset/sample.csv
+ * 2. It Translates the numeric class IDs to string based ones.
+ * 3. It freezes the model's weights inside the ./coronavirus-tweet-classifier.json
+ */
+const natural = require("natural");
+const csv = require("csvtojson");
+
+let persistModel = (classifier) => {
+    return new Promise((resolve, reject) => {
+        classifier.save("./coronavirus-tweet-classifier.json", (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+let loadData = () => {
+    return new Promise((resolve) => {
+        csv()
+            .fromFile("./dataset/Corona_NLP_train.csv")
+            .then((json) => {
+                resolve(json);
+            });
+    });
+};
+
+(async () => {
+    try {
+        let dataset = await loadData();
+        let classifier = new natural.BayesClassifier();
+
+        dataset.forEach((i) => {
+
+            classifier.addDocument(i.OriginalTweet.toLowerCase(), i.Sentiment);
+        });
+
+        classifier.train();
+
+        await persistModel(classifier);
+    } catch (err) {
+        console.log(err.message);
+        console.log(err.stack);
+    }
+})();
---
+++ 002_evaluate/app.js
@@ -0,0 +1,59 @@
+const natural = require("natural");
+const csv = require("csvtojson");
+const naiveBayes = natural.BayesClassifier;
+const MODEL_PATH = "../001_train/coronavirus-tweet-classifier.json";
+
+let loadFrozenModel = () => {
+    return new Promise((resolve, reject) => {
+        naiveBayes.load(MODEL_PATH, null, (err, classifier) => {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(classifier);
+        });
+    });
+};
+
+let loadData = () => {
+    return new Promise((resolve) => {
+        csv()
+            .fromFile("../001_train/dataset/Corona_NLP_test.csv")
+            .then((json) => {
+                resolve(json);
+            });
+    });
+};
+
+
+(async () => {
+    try {
+        let [classifier, data] = await Promise.all([loadFrozenModel(), loadData()]);
+        let results = data.map((i, idx) => {
+
+            let predicted = classifier.classify(i.OriginalTweet);
+
+            console.clear();
+            console.log(`Inferring Item ${idx + 1} out of ${data.length}`);
+
+            return {
+                text: i.OriginalTweet,
+                actual: i.Sentiment,
+                predicted,
+                isCorrect: i.Sentiment === predicted,
+            };
+        });
+        let accurateHit = results.filter((i) => i.isCorrect);
+        let missedItems = results.filter((i) => !i.isCorrect);
+        let accuracy = (accurateHit.length / data.length) * 100;
+        let errorRate = (missedItems.length / data.length) * 100;
+
+        console.log(`Accurate: ${accurateHit.length} items`);
+        console.log(`Missed: ${missedItems.length} items`);
+        console.log(`Accuracy: ${accuracy.toFixed(2)}`);
+        console.log(`Error Rate: ${errorRate.toFixed(2)}`);
+    } catch (err) {
+        console.log(err.message);
+        console.log(err.stack);
+    }
+})();
